0a8a17e386991378bd299837619293daf09d32a3
victor-silveir <vbf.silveira@gmail.com>
2021-03-18 00:38:53 -0300

    Create Button component

---
+++ src/components/basic components/button/styles.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+const ButtonCustom = styled.button`
+    width: 100%;
+    height: 3.5rem;
+    padding: 0 1rem;
+    border-radius: 0.625rem;
+    background-color: #a1a1a1;
+    border: 0;
+    font-weight: 600;
+    font-size: 0.9rem;
+    margin-top: 1rem;
+    transition: 0.5s ease;
+
+    &:hover {
+        background-color: #ff9000;
+    }
+`
+  
