37b433fa29545936034caab86f0b5c3a3888143e
victor-silveir <vbf.silveira@gmail.com>
2021-03-19 18:34:20 -0300

    Create CustomersList  component

---
+++ src/components/customers-list/index.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { Span } from "../basic components/span/styles";
+import { CostumersDiv, CustomersContent, CustomersItems } from "./styles";
+
+function CustomersList(props) {
+    return (
+        <CustomersContent>
+                    {props.clients.map((client) => (
+                        <CustomersItems key={client.id}>
+                            <Link href={`customers/${client.id}`}>
+                                <CostumersDiv>
+                                    <Span>Name: </Span>
+                                    <Span>{client.name}</Span>
+                                </CostumersDiv>
+                            </Link>
+                            <Link href={`customers/${client.id}`}>
+                                <CostumersDiv>
+                                    <Span>CPF: </Span>
+                                    <Span>{client.cpf}</Span>
+                                </CostumersDiv>
+                            </Link>
+                        </CustomersItems>
+                    ))}
+        </CustomersContent>
+    );
+};
+
+export default CustomersList;
