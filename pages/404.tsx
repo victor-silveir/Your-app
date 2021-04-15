c12c20a0a19e2856704af8fe16745b6705dc52ba
victor-silveir <vbf.silveira@gmail.com>
2021-04-15 17:09:53 -0300

    Add footer to 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import Background from "../src/components/basic components/background";
+import Footer from "../src/components/footer";
 import { Header, MobileHeader } from "../src/components/header";
 import { Content } from "./home/styles";
 
@@ -7,14 +8,15 @@ import { Content } from "./home/styles";
 function notFound() {
     return (
         <Background fixed backgroundImage='/img/background2.jpg'>
-                <Header />
-                <MobileHeader />
+            <Header />
+            <MobileHeader />
             <Content>
-            <motion.h1 animate={{ color: "#ff9000" }} transition={{ duration: 1 }}>404 - Page Not Found!</motion.h1>
-                        <h2>We can't find the page that you are looking for  </h2>
-                        <br></br>
-                        <h2>Return <a href='/home'>Home</a> !</h2>
+                <motion.h1 animate={{ color: "#ff9000" }} transition={{ duration: 1 }}>404 - Page Not Found!</motion.h1>
+                <h2>We can't find the page that you are looking for  </h2>
+                <br></br>
+                <h2>Return <a href='/home'>Home</a> !</h2>
             </Content>
+            <Footer />
         </Background>
     )
 };
