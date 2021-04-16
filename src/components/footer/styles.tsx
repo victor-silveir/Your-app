235cd6b3e5e3c162fdca0e0b0d422a9a68ea73bd
victor-silveir <vbf.silveira@gmail.com>
2021-04-16 18:01:22 -0300

    Fix Footer to be responsive

--- src/components/footer/styles.tsx
+++ src/components/footer/styles.tsx
@@ -10,7 +10,7 @@ export const Footr = styled.footer`
 export const FooterBar = styled.div`
     margin: 0 auto;
     max-width: 90rem;
-    background-color: black;
+    background-color: #312E38;
     border-top-left-radius: 10px;
     border-top-right-radius: 10px;
     display: flex;
@@ -28,11 +28,28 @@ export const Profilelogo = styled.img`
     height: 6.5rem;
     border-radius: 50%;
     margin-right: 1.5rem;
+
+    @media(max-width: 450px) {
+        width: 4rem;
+        height: 4rem;
+    }
+
+    @media(max-width: 400px) {
+        width: 3rem;
+        height: 3rem;
+    }
+
 `
 
 export const ProfileContent = styled.div` 
     display: flex;
     flex-direction: column;
+
+    @media(max-width: 400px) {
+        span {
+            font-size: 0.6rem;
+        }
+    }
 `
 
 export const SocialIcons = styled.div`
