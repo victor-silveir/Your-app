e6312a11cb1daee659d306a0176656cd87e1578f
victor-silveir <vbf.silveira@gmail.com>
2021-04-16 14:56:07 -0300

    Implement Authorization

--- pages/customers/[id].tsx
+++ pages/customers/[id].tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Background from "../../src/components/basic components/background";
 import { CustomerUpdateCard } from "../../src/components/customer-card";
 import { Header, MobileHeader } from "../../src/components/header";
@@ -13,6 +13,7 @@ import { mask } from 'remask'
 import { CustomerData } from "../../src/models/CustomerData";
 import  Router  from "next/router";
 import Footer from "../../src/components/footer";
+import { useAuth } from "../../src/hooks/AuthHook";
 export async function getServerSideProps(ctx) {
     const { id } = ctx.query;
     return {
@@ -25,20 +26,31 @@ export async function getServerSideProps(ctx) {
 function Customer({ id }) {
     const [isDisabled, setIsDisabled] = useState(true);
     const [isOpened, setIsOpened] = useState(false);
+    const [isAdmin, setIsAdmin] = useState(true);
     const { data, error } = useGet<CustomerData>(`clients/${id}`);
-    if (data) {
-        data.cpf = mask(data.cpf, ['999.999.999-99']);
-        data.zipCode = mask(data.zipCode, ['99999-999']);
-        data.phones.map((phone, index) => {
-            data.phones[index].stateCode = mask(data.phones[index].stateCode, ['(99)'])
-            data.phones[index].number = mask(data.phones[index].number, ['9999-9999', '99999-9999'])
-        });
-    }
-    
-    if(error?.response.status == 401)  {
-        alert("Session expired, please login again!");
-        Router.reload();
-    };
+    const { userName } = useAuth();
+
+    useEffect(() => {
+        
+        if(userName == "comum") {
+            setIsAdmin(false);
+        }
+
+        if (data) {
+            data.cpf = mask(data.cpf, ['999.999.999-99']);
+            data.zipCode = mask(data.zipCode, ['99999-999']);
+            data.phones.map((phone, index) => {
+                data.phones[index].stateCode = mask(data.phones[index].stateCode, ['(99)'])
+                data.phones[index].number = mask(data.phones[index].number, ['9999-9999', '99999-9999'])
+            });
+        }
+        
+        if(error?.response.status == 401)  {
+            alert("Session expired, please login again!");
+            Router.reload();
+        };
+    });
+
 
 
     const toggleForm = useCallback(() => {
@@ -51,7 +63,7 @@ function Customer({ id }) {
     if (!data) {
         return <></>
     }
-    console.log(data);
+
 
     return (
         <Background fixed backgroundImage='/img/background2.jpg'>
@@ -59,7 +71,8 @@ function Customer({ id }) {
             <MobileHeader />
             {isOpened && <Modal client={data} openModal={toggleDelete} />}
             <Container>
-                <CustomerUpdateCard enableDelete={toggleDelete} enableUpdate={toggleForm} />
+                {isAdmin && <CustomerUpdateCard enableDelete={toggleDelete} enableUpdate={toggleForm} />
+}
                 <UpdateCustomerForm initialvalues={data} isdisabled={isDisabled ? 1 : 0} isvisible={isDisabled ? 1 : 0} />
             </Container>
             <Footer />
--- pages/customers/index.tsx
+++ pages/customers/index.tsx
@@ -3,18 +3,28 @@ import { Container } from "./styles";
 import { Header, MobileHeader } from "../../src/components/header";
 import NewCustomerForm from "../../src/components/new-customer-form";
 import Background from "../../src/components/basic components/background";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import CustomersList from "../../src/components/customers-list";
 import { useGet } from "../../src/services/axios/api";
 import { CustomerData } from "../../src/models/CustomerData";
 import Footer from "../../src/components/footer";
+import { useAuth } from "../../src/hooks/AuthHook";
 
 function Customers() {
 
     const [isRendered, setIsRendered] = useState(true);
+    const [isAdmin, setIsAdmin] = useState(true);
+    const { userName } = useAuth();
+    console.log(userName);
     const { data } = useGet<CustomerData[]>('clients/');
     const handleRender = useCallback(() => {
         setIsRendered(current => !current)
+    }, [])
+    
+    useEffect(() => {
+        if (userName == "comum") {
+            setIsAdmin(false);
+        }
     }, [])
 
     return (
@@ -22,7 +32,8 @@ function Customers() {
             <Header />
             <MobileHeader />
             <Container>
-                <CustomerCard text={isRendered ? "Register new Customer" : "View Customers"} render={handleRender} />
+                {isAdmin && <CustomerCard text={isRendered ? "Register new Customer" : "View Customers"} render={handleRender} />
+                }
                 {isRendered ? <CustomersList clients={data} />
                     : <NewCustomerForm />
                 }
